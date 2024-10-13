f59e143ff779e88aa6beb22cd0469b18fe41f0b1
julia <juliafarganus@gmail.com>
2024-10-13 21:45:12 +0200

    figuring out splat alg

--- scripts/convert_to_ply.py
+++ scripts/convert_to_ply.py
@@ -2,11 +2,11 @@ import pycolmap
 from pathlib import Path
 
 # path to a folder with 'cameras.bin images.bin points3D.bin' files
-sparse_model = Path('../data/south-building/0')
+sparse_model = Path('../data/south-building-d4x/0')
 
-output_path = Path('../data/south-building')
+output_path = Path('../data/south-building-d4x')
 
 reconstruction = pycolmap.Reconstruction(sparse_model)
 
-reconstruction.export_PLY(output_path / 'sparse-2.ply')
+reconstruction.export_PLY(output_path / 'sparse.ply')
 
