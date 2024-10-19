2841cd95897a5768036693ddd4b51fe0fa5151c9
julia <juliafarganus@gmail.com>
2024-10-19 16:55:38 +0200

    try with another scene?

--- scripts/change_image_resolution.py
+++ scripts/change_image_resolution.py
@@ -5,8 +5,8 @@ from matplotlib import image as mpimg
 from skimage.transform import downscale_local_mean, resize
 from pathlib import Path
 
-input_folder = Path("../data/south-building/images")
-output_folder = Path("../data/south-building/images_downsampled")
+input_folder = Path("../data/small_city_road_outside/images")
+output_folder = Path("../data/small_city_road_outside-d4x/images")
 
 os.makedirs(output_folder, exist_ok=True)
 
@@ -15,6 +15,6 @@ factor = 4
 for img_path in input_folder.iterdir():
     img = mpimg.imread(img_path)
     print(f"Processing image {img_path.name}")
-    downscaled = downscale_local_mean(img, (factor, factor, 1)).astype(np.uint8)
+    downscaled = downscale_local_mean(img, (factor, factor, 1))
     mpimg.imsave(output_folder / img_path.name, downscaled)
 
--- scripts/small_splat_test.py
+++ scripts/small_splat_test.py
@@ -1,13 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from gsplat import rasterization
 # Initialize a 3D Gaussian :
-mean = torch.tensor([[0.0, 0.0, 0.0]], device="cuda:0")
-quat = torch.tensor([[1.0, 0.0, 0.0, 0.0]], device="cuda:0")
-color = torch.rand((1, 3), device="cuda:0")
-opac = torch.ones((1,), device="cuda:0")
-scale = torch.rand((1, 3), device="cuda:0")
-view = torch.eye(4, device="cuda:0")[None]
-K = torch.tensor([[[1.0, 0.0, 120.], [0., 1., 120.], [0., 0., 1.]]], device="cuda:0")
-rgb_image, alpha, metadata = rasterization(
-    mean, quat, color, opac, scale, view, K, 240, 240
+device = "cuda:0"
+means = torch.randn((100, 3), device=device)
+quats = torch.randn((100, 4), device=device)
+scales = torch.rand((100, 3), device=device) * 0.25
+colors = torch.rand((100, 3), device=device)
+opacities = torch.rand((100,), device=device)
+viewmats = torch.eye(4, device=device)[None, :, :]
+Ks = torch.tensor([
+   [300., 0., 150.], [0., 300., 100.], [0., 0., 1.]], device=device)[None, :, :]
+width, height = 300, 200
+colors, alphas, meta = rasterization(
+   means, quats, scales, opacities, colors, viewmats, Ks, width, height
 )
+img_np = colors.cpu()[0].numpy()
+print(np.mean(img_np))
+
+plt.imshow(img_np)
+plt.show()
