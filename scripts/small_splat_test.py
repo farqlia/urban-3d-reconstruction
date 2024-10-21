eeca7940dee46b8add49f307dc3cc23fb848e46a
julia <juliafarganus@gmail.com>
2024-10-21 20:59:52 +0200

    convert from torch to ply

--- scripts/small_splat_test.py
+++ scripts/small_splat_test.py
@@ -1,9 +1,11 @@
+import pandas as pd
 import torch
-import open3d as o3d
-import matplotlib.pyplot as plt
 import numpy as np
+import pyntcloud
 
-g_model = torch.load("../results/small_city_road_down_test/epochs_200_000/ckpts/ckpt_199999_rank0.pt", map_location="cpu", weights_only=True)
+g_model = torch.load("../results/south-building/ckpts/ckpt_6999_rank0.pt", map_location="cpu", weights_only=True)
 
-gs_cloud = o3d.geometry.PointCloud()
-gs_cloud.points = o3d.utility.Vector3dVector(g_model['splats']['means'].numpy()[:10])
+
+
+cloud = pyntcloud.PyntCloud(pd.DataFrame(points, columns=["x", "y", "z"]))
+
