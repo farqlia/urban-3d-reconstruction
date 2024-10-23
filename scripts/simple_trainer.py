5d675a33d85dcde196a58ce9d59567719833d1f7
julia <juliafarganus@gmail.com>
2024-10-23 17:58:42 +0200

    training splats

--- scripts/simple_trainer.py
+++ scripts/simple_trainer.py
@@ -1,7 +1,8 @@
 import time
+from dataclasses import field
+from typing import List
 
 import torch
-import tyro
 from gsplat.distributed import cli
 from gsplat.strategy import DefaultStrategy, MCMCStrategy
 
@@ -37,7 +38,8 @@ def main(local_rank: int, world_rank, world_size: int, cfg: Config):
         print("Viewer running... Ctrl+C to exit.")
         time.sleep(1000000)
 
-
+# TODO: configuration should be saved! (later for comparison)
+# TODO: what does batch size mean?
 if __name__ == "__main__":
     """
     Usage:
@@ -51,18 +53,50 @@ if __name__ == "__main__":
 
     """
 
+    init_type = "random"
+    strategy = "mcmc"
+    max_steps: int = 100_000
+
+    init_num_pts: int = 100_000 # only for random
+
+    data_dir = "../data/small_city_road_down_test"
+    result_dir = f"../results/small_city_road_down_test/{init_type}/{strategy}/epochs_{max_steps}"
+
+    eval_steps_mapping = {
+        100_000: [2000, 10_000, 30_000, 50_000, 60_000, 75_000, 100_000]
+    }
+
+    # Steps to evaluate the model
+    eval_steps: List[int] = eval_steps_mapping[max_steps]
+    # Steps to save the model
+    save_steps: List[int] = eval_steps_mapping[max_steps]
+
     # Config objects we can choose between.
     # Each is a tuple of (CLI description, config object).
     configs = {
         "default": (
             "Gaussian splatting training using densification heuristics from the original paper.",
             Config(
+                data_dir=data_dir,
+                result_dir=result_dir,
+                init_type=init_type,
+                max_steps=max_steps,
+                eval_steps=eval_steps,
+                init_num_pts=init_num_pts,
+                save_steps=save_steps,
                 strategy=DefaultStrategy(verbose=True),
             ),
         ),
         "mcmc": (
             "Gaussian splatting training using densification from the paper '3D Gaussian Splatting as Markov Chain Monte Carlo'.",
             Config(
+                data_dir=data_dir,
+                result_dir=result_dir,
+                init_type=init_type,
+                eval_steps=eval_steps,
+                save_steps=save_steps,
+                max_steps=max_steps,
+                init_num_pts=init_num_pts,
                 init_opa=0.5,
                 init_scale=0.1,
                 opacity_reg=0.01,
@@ -71,7 +105,7 @@ if __name__ == "__main__":
             ),
         ),
     }
-    cfg = configs["default"][1] # tyro.extras.overridable_config_cli(configs)
+    cfg = configs[strategy][1] # tyro.extras.overridable_config_cli(configs)
     cfg.adjust_steps(cfg.steps_scaler)
 
     # try import extra dependencies
