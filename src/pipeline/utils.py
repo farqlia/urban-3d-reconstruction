f58b7d0d1e6795a9698c24dfd62d32ac6bc2dd1a
Kasia <kasiawo02@gmail.com>
2024-10-31 17:39:30 +0100

    extract script running to utils

---
+++ src/pipeline/utils.py
@@ -0,0 +1,8 @@
+from subprocess import run, CalledProcessError
+from config import SCRIPTS_DIR
+
+def run_script(script_name, *args):
+    try:
+        run(["python", SCRIPTS_DIR / script_name, *args], check=True)
+    except CalledProcessError as e:
+        raise RuntimeError(f"Failed to execute {script_name})") from e
