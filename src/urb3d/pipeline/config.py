8c6de9589fa618c5ea8c95be7c9658bf902bc087
kasia <kasiawo02@gmail.com>
2024-11-06 23:46:41 +0100

    move module

--- src/urb3d/pipeline/config.py
+++ src/urb3d/pipeline/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parents[2] 
+BASE_DIR = Path(__file__).resolve().parents[3]
 INPUT_DATA_FOLDER = BASE_DIR / os.getenv("INPUT_DATA_FOLDER")
 DATA_FOLDER = BASE_DIR / os.getenv("DATA_FOLDER")
 SCRIPTS_DIR = BASE_DIR / "scripts"
