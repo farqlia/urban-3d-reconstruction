3560a7daf39a7ca83126588ecb8228b9cd57beb9
kasia <kasiawo02@gmail.com>
2024-11-07 00:02:18 +0100

    specify python executable

--- src/urb3d/pipeline/utils.py
+++ src/urb3d/pipeline/utils.py
@@ -1,8 +1,10 @@
 from subprocess import run, CalledProcessError
 from config import SCRIPTS_DIR
+import sys
 
 def run_script(script_name, *args):
+    python_executable = sys.executable
     try:
-        run(["python", SCRIPTS_DIR / script_name, *args], check=True)
+        run([python_executable, SCRIPTS_DIR / script_name, *args], check=True)
     except CalledProcessError as e:
         raise RuntimeError(f"Failed to execute {script_name})") from e
