42f2fce30039e404d45ac894fdfbfe419cddf49b
kasia <kasiawo02@gmail.com>
2024-11-06 23:39:50 +0100

    fix model saving

--- src/pipeline/model_processor.py
+++ src/pipeline/model_processor.py
@@ -1,4 +1,4 @@
-from config import INPUT_DATA_FOLDER, DATA_FOLDER, COLMAP_RECONSTRUCTION_DIR, GAUSSIAN_MODEL_PT, GAUSSIAN_MODEL_PLY
+from config import INPUT_DATA_FOLDER, DATA_FOLDER, COLMAP_RECONSTRUCTION_DIR, CKPTS_PATH, GAUSSIAN_MODEL_PLY
 from utils import run_script
 
 class ModelProcessor():
@@ -8,20 +8,21 @@ class ModelProcessor():
         self.output_folder = DATA_FOLDER
         self.output_folder.mkdir(parents=True, exist_ok=True)
         self.reconstruction_folder = COLMAP_RECONSTRUCTION_DIR
-        self.gaussian_pt_path = GAUSSIAN_MODEL_PT
+        self.ckpts_path = CKPTS_PATH
         self.gaussian_ply_path = GAUSSIAN_MODEL_PLY
 
     def _reconstruct_point_cloud(self):
-        run_script("colmap_reconstruction.py", "--input", str(self.input_folder), 
-                   "--output", str(self.output_folder))
+        if not self.reconstruction_folder.exists():
+            run_script("colmap_reconstruction.py", "--input", str(self.input_folder),
+                       "--output", str(self.output_folder))
 
     def _create_gaussian_model(self): 
         if not self.gaussian_ply_path.exists():
-            if not self.gaussian_pt_path.exists():
+            if not self.ckpts_path.exists():
                 run_script("simple_trainer.py", "--data_dir", str(self.reconstruction_folder),  #?
                            "--result_dir", str(self.output_folder))        
                 
-            run_script("torch_model_to_ply.py", "--input", str(self.gaussian_pt_path), 
+            run_script("save_model.py", "--ckpts", str(self.ckpts_path),
                        "--output", str(self.gaussian_ply_path))
         
     def run_full_reconstruction(self):
