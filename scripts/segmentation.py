f5defdeb5308d1168f69b78d7c06f91ba44c41f6
kasia <kasiawo02@gmail.com>
2024-11-08 18:13:22 +0100

    add run function

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -1,14 +1,29 @@
 from pyntcloud import PyntCloud
-import numpy as np
 import argparse
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from urb3d.segmentation.segmentor import PointNetSegmentor
+from urb3d.segmentation.dataset import PointCloudSegmentationDataset
+import numpy as np
 
-def save_class_labels_in_ply(input_ply_path, output_ply_path, class_labels):
-    cloud = PyntCloud.from_file(input_ply_path)
+def run(segmentor_ckpt_path, input_ply_path, output_ply_path):
+    model = PointNetSegmentor.load_from_checkpoint(segmentor_ckpt_path)
+    dataset = PointCloudSegmentationDataset(input_ply_path)
+    data_loader = DataLoader(dataset)
+    trainer = pl.Trainer()
+
+    preds = trainer.predict(model, data_loader)[0][0].numpy()
+
+    save_class_labels_in_ply(dataset.pt, output_ply_path, preds) # sampled point cloud instead of pt
+
+
+def save_class_labels_in_ply(cloud, output_ply_path, class_labels):
     data = cloud.points
-    
+    print(class_labels)
     if len(class_labels) != len(data):
         raise ValueError("Number of class labels must match the number of points in the .ply file.")
 
+    class_labels = np.array(class_labels, dtype=np.int32)
     data['class_label'] = class_labels
 
     new_cloud = PyntCloud(data)
@@ -17,18 +32,13 @@ def save_class_labels_in_ply(input_ply_path, output_ply_path, class_labels):
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, help="Path to the input .ply file.")
-    parser.add_argument("--output", type=str, help="Path to save the modified .ply file after segmentation.")
+    parser.add_argument("--ckpt", type=str, required=True, help="Path to the checkpoint .ckpt file.")
+    parser.add_argument("--input", type=str, required=True, help="Path to the input .ply file.")
+    parser.add_argument("--output", type=str, required=True, help="Path to save the modified .ply file after segmentation.")
 
     args = parser.parse_args()
 
-    input = args.input                                              ## 
-    cloud = PyntCloud.from_file(input)                              ##
-    print(cloud.points)                                             ##
-    output = args.output                                            ##
-    class_labels = np.random.randint(1, 6, size=len(cloud.points))  ## 
+    run(args.ckpt, args.input, args.output)
 
-    save_class_labels_in_ply(input, output, class_labels)
-
-    cloud = PyntCloud.from_file(output)                             ##
-    print(cloud.points)                                             ##
+    cloud = PyntCloud.from_file(args.output)                             ##
+    print(cloud.points)                                                  ##
