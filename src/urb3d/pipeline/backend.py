1d728cf8f4cf10a83e0625e9b25886d60557d0c6
kwochal <kasiawo02@gmail.com>
2024-12-09 13:03:34 +0100

    make backend class a QObject

--- src/urb3d/pipeline/backend.py
+++ src/urb3d/pipeline/backend.py
@@ -1,10 +1,14 @@
 from src.urb3d.pipeline.file_manager import FileManager
 from src.urb3d.pipeline.model_processor import ModelProcessor
 from src.urb3d.pipeline.segmentation_processor import SegmentationProcessor
+from src.urb3d.pipeline.config import POINT_CLOUD_SPARSE, GAUSSIAN_MODEL_PLY, SEGMENTED_PLY_PATH
+from PySide6.QtCore import QObject, Slot
 
-class Backend:
+
+class Backend(QObject):
 
     def __init__(self):
+        super().__init__()
         self.model_processor = ModelProcessor()
         self.segmentation_processor = SegmentationProcessor()
         self.file_manager = FileManager()
