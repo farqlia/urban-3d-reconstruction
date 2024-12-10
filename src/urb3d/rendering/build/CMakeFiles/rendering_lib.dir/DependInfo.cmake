d0160c4be999dd72070e59cd1004c0fd762cfc9f
Rafał Mielniczuk <rmielniczuk001@gmail.com>
2024-12-10 01:30:57 +0100

    added lib

---
+++ src/urb3d/rendering/build/CMakeFiles/rendering_lib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/camera.c" "CMakeFiles/rendering_lib.dir/src/camera.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/camera.c.o.d"
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/data.c" "CMakeFiles/rendering_lib.dir/src/data.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/data.c.o.d"
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/loader.c" "CMakeFiles/rendering_lib.dir/src/loader.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/loader.c.o.d"
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/main.c" "CMakeFiles/rendering_lib.dir/src/main.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/main.c.o.d"
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/rendering.c" "CMakeFiles/rendering_lib.dir/src/rendering.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/rendering.c.o.d"
+  "/home/frafau/Documents/urban-3d-reconstruction/src/rendering/src/shader.c" "CMakeFiles/rendering_lib.dir/src/shader.c.o" "gcc" "CMakeFiles/rendering_lib.dir/src/shader.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/urb3d/rendering/build/CMakeFiles/rendering_lib.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rendering_lib.dir/src/camera.c.o"
+  "CMakeFiles/rendering_lib.dir/src/camera.c.o.d"
+  "CMakeFiles/rendering_lib.dir/src/data.c.o"
+  "CMakeFiles/rendering_lib.dir/src/data.c.o.d"
+  "CMakeFiles/rendering_lib.dir/src/loader.c.o"
+  "CMakeFiles/rendering_lib.dir/src/loader.c.o.d"
+  "CMakeFiles/rendering_lib.dir/src/main.c.o"
+  "CMakeFiles/rendering_lib.dir/src/main.c.o.d"
+  "CMakeFiles/rendering_lib.dir/src/rendering.c.o"
+  "CMakeFiles/rendering_lib.dir/src/rendering.c.o.d"
+  "CMakeFiles/rendering_lib.dir/src/shader.c.o"
+  "CMakeFiles/rendering_lib.dir/src/shader.c.o.d"
+  "librendering_lib.pdb"
+  "librendering_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rendering_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
